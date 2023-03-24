a3d6f897c309e74c50d3753bd2878ea5bc9c75b3
Christopher Lowe <christopher.lowe@strath.ac.uk>
2023-03-24 15:22:08 +0000

    Ground station passes now stored

--- main.py
+++ main.py
@@ -9,10 +9,25 @@ from skyfield.api import wgs84, load, Time
 class Image:
 	"""Image base class"""
 	def __init__(
-			self, time_capture: Time, satellite: str, cloud: float = 0.):
+			self, target: str, time_capture: Time, satellite: str, cloud: float = 0.
+	):
+		self.target = target
 		self.time = time_capture
 		self.satellite = satellite
 		self.cloud = cloud
+
+
+class Download:
+	"""Download event base class"""
+	def __init__(self, ground_station: str, start: Time, end: Time, satellite: str):
+		self.ground_station = ground_station
+		self.start = start
+		self.end = end
+		self.satellite = satellite
+
+	@property
+	def duration(self):
+		return 24 * 60 * 60 * (self.end - self.start)
 
 
 if __name__ == "__main__":
@@ -32,15 +47,10 @@ if __name__ == "__main__":
 	}
 
 	# List of Ground Station objects to which images are downloaded
-	ground_stations = [
-		{
-			"name": "North Pole",
-			"location": wgs84.latlon(90.0, 0.0)
-		}, {
-			"name": "North Pole",
-			"location": wgs84.latlon(-90.0, 0.0)
-		}
-	]
+	ground_stations = {
+		"North Pole": wgs84.latlon(90.0, 0.0),
+		"South Pole": wgs84.latlon(-90.0, 0.0)
+	}
 
 	# Time horizon parameters, between which image capture and download events are found
 	t0 = load.timescale().utc(2023, 3, 24)
@@ -50,14 +60,36 @@ if __name__ == "__main__":
 	# (using their ID), and the value is an empty list that will get populated with
 	# discrete image events and their associated information
 	image_events = {target: [] for target in targets}
+	download_events = {gs: [] for gs in ground_stations}
 
-	for target, location in targets.items():
-		for satellite in satellites:
+	for satellite in satellites:
+		for target, location in targets.items():
 			# Get the rise, culmination and fall for all passes between this
 			# satellite:target pair during the time horizon
 			t, events = satellite.find_events(location, t0, t1, altitude_degrees=30.0)
 			for ti, event in zip(t, events):
-				if event == 1:  # If this is the "peak" of the pass
-					image_events[target].append(Image(ti, satellite.name))
+				if event != 1:  # If this is NOT the "peak" of the pass
+					continue
+				image_events[target].append(
+					Image(target, ti, satellite.name)
+				)
+
+		t_rise = None
+		for gs, location in ground_stations.items():
+			t, events = satellite.find_events(location, t0, t1, altitude_degrees=10.0)
+			for ti, event in zip(t, events):
+				# If the event is 0 (i.e. "rise"), store the rise event, else if it's
+				# 0 (i.e. "culmination"), continue
+				if event == 0:
+					t_rise = ti
+					continue
+				if event == 1:
+					continue
+				if not isinstance(t_rise, Time):
+					continue
+				# So long as a rise time has been defined, instantiate the Download event
+				download_events[gs].append(
+					Download(gs, t_rise, ti, satellite.name)
+				)
 
 	print('')
